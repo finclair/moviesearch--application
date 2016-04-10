2e78cfe54ba03bdcc8b33b64e05ba8c49203d828
Klaus Heino <klaus.heino@gmail.com>
2016-04-10 18:55:50 +0300

    shortened code by cleaning and refactoring..

--- movie-list.js
+++ movie-list.js
@@ -1,33 +1,26 @@
-
-//the event listener is placed to search button 
+//the event listener is placed to search button
 document.getElementById('search-button').addEventListener('click', function () {
-    console.log('Listing movies..');
-	
-	var searchWord = document.getElementById('movie-input').value;
-	var httpRequest;
-	
-	var omdbBaseUrlList = 'http://www.omdbapi.com/?s='
-	var UrlForList = omdbBaseUrlList + searchWord;
+	var searchWord = document.getElementById('movie-input').value.trim();
+	var omdbBaseUrlList = 'http://www.omdbapi.com/?s=';
+	var urlForList = omdbBaseUrlList + searchWord;
 
     if (searchWord) {
-        fetchMovieData(UrlForList, logMovies);
+        fetchMovieData(urlForList, logMovies);
     }
 
     function fetchMovieData(url, callback) {
-		console.log('used the function fetchMovieData');
-        httpRequest = new XMLHttpRequest();
+        var httpRequest = new XMLHttpRequest();
 
         httpRequest.onreadystatechange = function() {
 			if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-				callback();
+				callback(JSON.parse(httpRequest.responseText));
 			}
 		};
         httpRequest.open('GET', url);
         httpRequest.send();
     }
 
-    function logMovies() {
-		var movieList = JSON.parse(httpRequest.responseText);
+    function logMovies(movieList) {
 		console.log(movieList);
 
 		//this is for looping the results from parsed JSON -array
@@ -50,18 +43,11 @@ document.getElementById('search-button').addEventListener('click', function () {
 
 				fetchMovieData(UrlForSingle, showMovieData);
 
-				function showMovieData() {
-					var movie = JSON.parse(httpRequest.responseText);
-					var rated = JSON.stringify(movie.Rated);
-					var title = JSON.stringify(movie.Title);
-					var year = JSON.stringify(movie.Year);
-					var rated = JSON.stringify(movie.Rated);
-					var plot = JSON.stringify(movie.Plot);
-
-					document.getElementById('title-of-movie').innerHTML = title;
-					document.getElementById('year-of-movie').innerHTML = year;
-					document.getElementById('rate-of-movie').innerHTML = rated;
-					document.getElementById('plot-of-movie').innerHTML = plot;
+				function showMovieData(movie) {
+					document.getElementById('title-of-movie').innerHTML = movie.Title;
+					document.getElementById('year-of-movie').innerHTML = movie.Year;
+					document.getElementById('rate-of-movie').innerHTML = movie.Rated;
+					document.getElementById('plot-of-movie').innerHTML = movie.Plot;
 				}
 			}
 		});
